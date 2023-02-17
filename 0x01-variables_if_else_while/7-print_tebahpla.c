62c588d36c88136561d0974ddc78a2d3da90c103
Axis44 <kunenexolani34@gmail.com>
2023-02-17 04:50:58 -0800

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+
+/**
+ *
+ *
+ */
+
+int main(void)
+{
+
+	char low;
+
+	for (low = 'z'; low >='a'; low --)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}	
