5d0a430c2240073f4db2ab25a4e0f939b0122c68
Axis44 <kunenexolani34@gmail.com>
2023-02-17 04:44:24 -0800

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}			
