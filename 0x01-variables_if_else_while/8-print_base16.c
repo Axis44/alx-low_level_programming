92db44a1536f0c0a3c65b23c054636ffa12ac02a
Axis44 <kunenexolani34@gmail.com>
2023-02-17 04:59:25 -0800

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ *
+ */
+
+int main(void)
+
+{
+
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	pitchar(low);
+	putchar('\n');
+
+	return (0)
+
+}		
