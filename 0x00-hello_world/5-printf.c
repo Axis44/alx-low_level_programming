382de72840450fb5b2850916c0022c242a8eaaec
Axis44 <kunenexolani34@gmail.com>
2023-02-17 01:40:34 -0800

    first printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
-*main fuction printing
-*return: 0
+*main - print c program using printf
+*
+*Return: 0
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 return (0);
 }
