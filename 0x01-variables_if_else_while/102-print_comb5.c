15d9b97ac666e838bd61f6abc78641dd050be0dc
Axis44 <kunenexolani34@gmail.com>
2023-02-17 19:51:48 -0800

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of single digit
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int l_d;
+
+	int c2;
+	int f_d2;
+	int l-d2;
+
+	while (c <= 98)
+	{
+		f_d = (c /10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
