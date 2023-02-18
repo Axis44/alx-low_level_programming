affff83ab65e73e6cb5c8faec6aacd55f2a6b954
Axis44 <kunenexolani34@gmail.com>
2023-02-18 05:58:05 -0800

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <unistd>
 
 /**
  * main - entry point
- *
+ *prints two digits combinations
  * Return: 0 (success)
  */
 
@@ -12,7 +11,23 @@ int main(void)
 {
 	int d, p;
 
-	for (d = '0'; d < '9'; d++)
+	for (d = '0', d <= '9', d++)
 	{
-	for (p = d + 1; p <= '9'; p++)
+	for (p = '0', p <= '9'; p++)
 	{
+	if (d < p)
+	{
+	putchar(d);
+	putchar(p);
+
+	if (d != '8' || (c == '8' && p != '9'))
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar("\n");
+	return (0);
+}
