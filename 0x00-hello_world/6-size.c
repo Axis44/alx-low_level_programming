72f5d9e48e4427a023e8f87b0194ce061450f3a4
Axis44 <kunenexolani34@gmail.com>
2023-02-19 04:53:15 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of char: %lu bytes\n", (unsigned long)sizeof(a));
 printf("Size of int: %lu bytes\n", (unsigned long)sizeof(b));
 printf("size of long int: %lu bytes\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu bytes\n", (unsigned long)sizeof(d));
-printf("Size of float: %lu byte\n", (unsigned long)sizeof(e));
+printf("Size of float: %lu byte\n", (unsigned long)sizeof(f));
 return (0);
 }
