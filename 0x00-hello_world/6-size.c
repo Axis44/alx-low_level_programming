4cadd2e2a2c48ba8103e8fb566e58963f7a3d17f
Axis44 <kunenexolani34@gmail.com>
2023-02-19 02:18:09 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
+char charType;
 int intType;
-float floatType;
 long longType;
 double doubleType;
-char charType;
+float floatType;
+printf("Size of char: %zu bytes\n", sizeof(charType));
 printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("size of long: %zu bytes\n", sizeof(longType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of float: %zu byte\n", sizeof(floatType));
 return (0);
 }
