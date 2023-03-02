e3ca64ac69fbc552a7388d101caadaef8d92138f
Axis44 <kunenexolani34@gmail.com>
2023-03-02 03:32:32 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int n;
+	int m;
+char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		for (n = 0; n < 52; n++)
+		{
+			if (s[m] == data1[n])
+			{
+				s[m] = datarot[n];
+				break;
+			}
+		}
+	}
+	return (s);
+}
