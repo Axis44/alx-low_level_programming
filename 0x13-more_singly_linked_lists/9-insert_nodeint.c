b3779e4acdd191be8e6d6a1600416fbe42437ec6
Axis44 <kunenexolani34@gmail.com>
2023-04-05 02:11:32 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 {
-	unsigned int t;
+	unsigned int i;
 	listint_t *new;
 	listint_t *temp = *head;
 
@@ -29,9 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 		return (new);
 	}
 
-	for (t = 0; temp && t < idx; t++)
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (t == idx - 1)
+		if (i == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;
