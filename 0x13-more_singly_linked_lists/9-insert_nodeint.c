9db5e1bdf04166a26c4f7efc7ce5b44bf01cfe67
Axis44 <kunenexolani34@gmail.com>
2023-04-05 02:38:43 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,10 +6,10 @@
  * @idx: index where node is added
  * @n: data to insert new node
  *
- * Return: pointer to new node or NULL
+ * Return: pointer to the new node or NULL
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
@@ -37,8 +37,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 			temp->next = new;
 			return (new);
 		}
+
 		else
 			temp = temp->next;
 	}
-	return (NULL)
+
+	return (NULL);
 }
