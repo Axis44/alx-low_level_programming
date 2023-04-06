f1a30613b0fd9497d3d7212f5d894896aee2a60c
Axis44 <kunenexolani34@gmail.com>
2023-04-06 14:30:19 -0700

    binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary representation in decimal
+ * @n: number binary prints
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int r, count = 0;
+	unsigned long int current;
+
+	for (r = 63; r >= 0; r--)
+
+	{
+		current = n >> r;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
