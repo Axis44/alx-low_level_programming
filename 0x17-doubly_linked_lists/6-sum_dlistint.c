4bc72f85a5ed60e6cde76df30ed6a86c31c27d54
Axis44 <kunenexolani34@gmail.com>
2023-08-17 08:14:47 -0700

    sum data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of data
+ * @head: head list
+ * Return: sum of the data, success
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+
+	return (sum);
+}
